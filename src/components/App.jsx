1411347dcd85851439b6f4969f7aef432fbf0ea9
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-10 12:20:06 +0300

    map notes from array of objects

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from '../notes';
 
 function App(){
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map((note)=> <Note noteTitle={note.title} noteContent={note.content} />)}
             <Footer />
         </div>
     )
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Note(){
+function Note(props){
     return (
         <div className='note'>
-            <h1>Note title</h1>
-            <p>note contnet</p>
+            <h1>{props.noteTitle}</h1>
+            <p>{props.noteContent}</p>
         </div>
     )
 }
